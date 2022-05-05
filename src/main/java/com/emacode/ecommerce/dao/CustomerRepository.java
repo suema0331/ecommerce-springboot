c86d23216e9a9efa8911b7fa9788a92d0db05bf6
Haruno Suematsu <haruno.s31203@gmail.com>
2022-05-05 20:08:50 +0900

    add CustomerRepository

--- src/main/java/com/emacode/ecommerce/dao/CustomerRepository.java
+++ src/main/java/com/emacode/ecommerce/dao/CustomerRepository.java
@@ -3,5 +3,12 @@ package com.emacode.ecommerce.dao;
 import com.emacode.ecommerce.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+// NOT annotated hence it will not be exposed as REST API
+// http://localhost:8080/api/cutomers
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    // Spring will execute a query similar to "SELECT * FROM Customer c WHERE c.email = theEmail"
+    // is not found returns null
+    Customer findByEmail(String theEmail);
 }
